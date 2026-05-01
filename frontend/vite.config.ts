99f0a717f7533a1b3b915905203cbac177a3c2b0
Code-with-ani <kawleaniruddh981@gmail.com>
2026-05-01 18:44:58 +0530

    Add allowedHosts to Vite server configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,15 +5,17 @@ import { createExpressApp, setupSocketIO } from "./server";
 import { Server as SocketIOServer } from "socket.io";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    fs: {
-      allow: ["./client", "./shared", "index.html"],
-      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
-    },
+server: {
+  host: "::",
+  port: 8080,
+  allowedHosts: [
+    "collaborative-code-editor-2718.onrender.com"
+  ],
+  fs: {
+    allow: ["./client", "./shared", "index.html"],
+    deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
   },
+},
   build: {
     outDir: "dist/spa",
   },
